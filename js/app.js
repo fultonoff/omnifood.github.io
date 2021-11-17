f39ee70cafd13ea9bfc8ea71e286743a63dab7ec
fulton <fultonkilebe7@gmail.com>
2021-11-17 22:44:52 +0200

    mobile respond

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// const iconClose = document.querySelector('.icon-close');
+// const iconOpen = document.querySelector('.icon-open');
+// const mainMenu = document.querySelector('.main-nav');
+
+
+
+// let nav = ()=>{
+//     mainMenu.classList.toggle('nav-open');
+// }
+
+// iconOpen.addEventListener('click', ()=>{
+//     nav()
+
+//     iconOpen.style.display = 'none';
+//     iconClose.style.display = 'block';
+// })
+
+// iconClose.addEventListener('click', ()=>{
+//     nav()
+
+//     iconOpen.style.display = 'block';
+//     iconClose.style.display = 'none';
+// })

f4ec0b48443fc8cb8bba7ea8a43b527be780b011
fulton <fultonkilebe7@gmail.com>
2021-11-21 00:14:38 +0200

    nav behavior

--- js/app.js
+++ js/app.js
@@ -7,3 +7,34 @@ const headerEl = document.querySelector('.header');
 btnNav.addEventListener('click', ()=>{
     headerEl.classList.toggle('nav-open')
 })
+
+////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function(link){
+    link.addEventListener('click', function(e){
+        e.preventDefault();
+        const href = link.getAttribute('href');
+        
+        // Scroll back top the top
+
+        if(href === '#') window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+
+        // Scroll to other links
+        if (href !== '#' && href.startsWith('#')){
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({behavior: 'smooth'})
+        }
+
+        //Close mobile navigation
+
+        if(link.classList.contains('main-nav-link')){
+            headerEl.classList.toggle('nav-open')
+        }
+    })
+})

bbade515704a9ee8aedbfed7513ef64e41bc211c
fulton <fultonkilebe7@gmail.com>
2021-11-21 12:49:54 +0200

    nav sticky

--- js/app.js
+++ js/app.js
@@ -1,40 +1,65 @@
 ///////////////////////////////////////////////////
 // Make mobile navigation work
 
-const btnNav = document.querySelector('.btn-mobile-nav');
-const headerEl = document.querySelector('.header');
+const btnNav = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
-btnNav.addEventListener('click', ()=>{
-    headerEl.classList.toggle('nav-open')
-})
+btnNav.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
 
 ////////////////////////////////////
 // Smooth scrolling animation
 
-const allLinks = document.querySelectorAll('a:link');
+const allLinks = document.querySelectorAll("a:link");
 
-allLinks.forEach(function(link){
-    link.addEventListener('click', function(e){
-        e.preventDefault();
-        const href = link.getAttribute('href');
-        
-        // Scroll back top the top
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
 
-        if(href === '#') window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        })
+    // Scroll back top the top
 
-        // Scroll to other links
-        if (href !== '#' && href.startsWith('#')){
-            const sectionEl = document.querySelector(href);
-            sectionEl.scrollIntoView({behavior: 'smooth'})
-        }
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
 
-        //Close mobile navigation
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
 
-        if(link.classList.contains('main-nav-link')){
-            headerEl.classList.toggle('nav-open')
-        }
-    })
-})
+    //Close mobile navigation
+
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+//////////////////////////
+//  Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    console.log(ent);
+    if(ent.isIntersecting === false){
+        document.body.classList.add('sticky')
+    }else{
+        document.body.classList.remove('sticky')
+    }
+  },
+
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px'
+  }
+);
+
+obs.observe(sectionHeroEl);
